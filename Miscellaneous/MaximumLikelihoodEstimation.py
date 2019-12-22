46603cedda7cffd35ee5966d07a955bb49d5c3c6
Moritz Wilksch <moritzwilksch@gmail.com>
2019-12-22 18:45:58 +0100

    added markov chain correction

--- Miscellaneous/MaximumLikelihoodEstimation.py
+++ Miscellaneous/MaximumLikelihoodEstimation.py
@@ -7,15 +7,17 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 # %%
-from scipy.stats import norm, rv_continuous
-data = np.array([1, 2, 5, 6])  # test data
-
+from scipy.stats import norm
+# data = np.array([1, 2, 5, 6])  # test data
+data = np.random.normal(1, 0.5, 20)
 
 # %%
+
+
 def get_lik(data, mu, sd):
     """ Returns likelihood of observing *data* given *mu* and *sigma* of a normal distribution """
-    nd = norm(mu, sd)
-    return np.prod(nd.pdf(data))
+
+    return np.prod(norm.pdf(data, loc=mu, scale=sd))
 
 
 # %%
@@ -32,10 +34,30 @@ for i in range(1000):
     old_lik = get_lik(data, mu[-1], sigma[-1])
     new_lik = get_lik(data, new_mu, new_sigma)
 
-    # if new likelihood > old likelihood, update params
-    if new_lik > old_lik:
+    if np.random.random() < (new_lik/old_lik):
+        # Markov Chain Element: Probability of updating
+        # depends on *how much better* the new parameters are
         mu.append(new_mu)
         sigma.append(new_sigma)
     else:  # keep old params
         mu.append(mu[-1])
         sigma.append(sigma[-1])
+
+# %% [markdown]
+""" 
+The markov chain element is in the if statement. In a markov chain, the probability of the parameter update always depends on how much the update would improve the parameters. In this case: If there is an improvement, always update. If there is not, do a coinflip, depending on which the paramters are still updated.
+"""
+
+# %%
+plt.plot(mu, label="mu")
+plt.plot(sigma, label="sigma")
+plt.legend()
+plt.show()
+
+sns.distplot(mu)
+plt.title("Mu")
+plt.show()
+sns.distplot(sigma)
+plt.title("Sigma")
+plt.show()
+# %%
