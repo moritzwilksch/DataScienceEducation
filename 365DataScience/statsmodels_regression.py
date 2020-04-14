bbbcb8ec192dcd113688fd6e54ec8d2d9e4f173d
Moritz Wilksch <moritzwilksch@gmail.com>
2020-04-14 13:31:19 +0200

    added sklearn pvalues

--- 365DataScience/statsmodels_regression.py
+++ 365DataScience/statsmodels_regression.py
@@ -1,9 +1,10 @@
-#%%
+# %%
 import seaborn as sns
 import pandas as pd
 import statsmodels.api as sm
+import numpy as np
 
-#%%
+# %%
 df = sns.load_dataset('tips')
 df = pd.get_dummies(df, drop_first=True)
 df.head()
@@ -11,13 +12,22 @@ df.head()
 X = df.drop('tip', axis=1)
 y = df.tip
 
-#%%
+# %%
 model = sm.OLS(y, X)
 results = model.fit()
 results.summary()
 
-#%%
+# %%
+# Regression in sklearn
 from sklearn.linear_model import LinearRegression
+
 lr = LinearRegression(normalize=True)
 lr.fit(X, y)
 pd.Series(lr.coef_, index=X.columns.values)
+
+# %%
+# Using f_regression for p-values for single features
+from sklearn.feature_selection import f_regression
+
+fr = f_regression(X, y)  # Outputs array of shape (2, n_features) with f-values (first) and p-values (second)
+print(pd.Series(np.round(fr[1], 3), index=X.columns, name='p_value'))
