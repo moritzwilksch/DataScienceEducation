0c3e761efecbc7f5a00846a4da8d9126c5980a90
Moritz Wilksch <moritzwilksch@gmail.com>
2020-08-12 17:33:39 +0200

    Updated MetropolisSampling

--- Miscellaneous/Statistics/MetropolisSampling.py
+++ Miscellaneous/Statistics/MetropolisSampling.py
@@ -16,7 +16,6 @@ data = np.random.normal(1, 0.5, 20)
 
 def get_lik(data, mu, sd):
     """ Returns likelihood of observing *data* given *mu* and *sigma* of a normal distribution """
-    breakpoint()
     return np.prod(norm.pdf(data, loc=mu, scale=sd))
 
 
@@ -28,14 +27,14 @@ sigma = [3]
 stepsize = 0.1
 
 for i in range(1000):
-    # Randomly propose an updated mu and sigma
+    # Randomly propose an updated mu and sigma (Metropolis sampling)
     new_mu = mu[-1] + np.random.normal(0, stepsize, 1)[0]
     new_sigma = sigma[-1] + np.random.normal(0, stepsize, 1)[0]
     old_lik = get_lik(data, mu[-1], sigma[-1])
     new_lik = get_lik(data, new_mu, new_sigma)
 
     if np.random.random() < (new_lik/old_lik):
-        # Markov Chain Element: Probability of updating
+        # Probability of updating (except when the new_mu is better, then always upgrade)
         # depends on *how much better* the new parameters are
         mu.append(new_mu)
         sigma.append(new_sigma)
@@ -45,7 +44,8 @@ for i in range(1000):
 
 # %% [markdown]
 """ 
-The markov chain element is in the if statement. In a markov chain, the probability of the parameter update always depends on how much the update would improve the parameters. In this case: If there is an improvement, always update. If there is not, do a coinflip, depending on which the paramters are still updated.
+The probability of the parameter update always depends on how much the update would improve the parameters. In this case: If there is an improvement, always update. If there is not, do a coinflip, depending on which the paramters are still updated.
+The Markov Chain part is that we jump to a different position with a propability p.
 """
 
 # %%
