e75125d1708b2bd54bc17f5dbeb721c9bf39a183
Moritz Wilksch <moritzwilksch@gmail.com>
2020-09-08 13:02:25 +0200

    changed centering

--- FastAI/LRFinder-FI-PDPBox.py
+++ FastAI/LRFinder-FI-PDPBox.py
@@ -97,14 +97,20 @@ pdp_smoker = pdp.pdp_isolate(model=wrapped,
                           model_features=x_cols,
                           feature=['smoker_No', 'smoker_Yes'])
 
+pdp_time = pdp.pdp_isolate(model=wrapped,
+                          dataset=pd.DataFrame(xtrain, columns=x_cols),
+                          model_features=x_cols,
+                          feature=['time_Lunch', 'time_Dinner'])
+
 pdp_bill = pdp.pdp_isolate(model=wrapped,
                           dataset=pd.DataFrame(xtrain, columns=x_cols),
                           model_features=x_cols,
-                          feature='total_bill', num_grid_points=50)
+                          feature='total_bill', num_grid_points=50,)
 
 
-pdp.pdp_plot(pdp_day, 'days', plot_lines=True,)
-pdp.pdp_plot(pdp_size, 'size', plot_lines=True,)
-pdp.pdp_plot(pdp_bill, 'total bill', plot_lines=True,)
-pdp.pdp_plot(pdp_sex, 'sex', plot_lines=True,)
-pdp.pdp_plot(pdp_smoker, 'smoker', plot_lines=True,)
+pdp.pdp_plot(pdp_day, 'days', plot_lines=True, center=False)
+pdp.pdp_plot(pdp_size, 'size', plot_lines=True, center=False)
+pdp.pdp_plot(pdp_bill, 'total bill', plot_lines=True, center=False)
+pdp.pdp_plot(pdp_sex, 'sex', plot_lines=True, center=False)
+pdp.pdp_plot(pdp_smoker, 'smoker', plot_lines=True, center=False)
+pdp.pdp_plot(pdp_time, 'time', plot_lines=True, center=True)
