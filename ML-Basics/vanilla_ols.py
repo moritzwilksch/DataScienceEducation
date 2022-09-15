ebec967f02c6280b931ab2ccd8877ae6f33b5af0
Moritz Wilksch <moritzwilksch@gmail.com>
2022-09-15 12:28:24 +0000

    cleanup, experimentation

--- ML-Basics/vanilla_ols.py
+++ ML-Basics/vanilla_ols.py
@@ -9,22 +9,20 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
 import numpy as np
 from sklearn.metrics import mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import HistGradientBoostingRegressor
+from sklearn.dummy import DummyRegressor
+from lightgbm import LGBMRegressor
 
 #%%
 
 df = sns.load_dataset("tips")
 
 # Feature Engineering
-df = df.assign(weekend=df["day"].isin(["Sat", "Sun"]))
+df = df.assign(tip_pct=df["tip"] / df["total_bill"])
 
-X = df.drop("tip", axis=1)
-# y = np.log(df["tip"])
-y = df["tip"]
-
-#%%
-sns.boxplot(data=df, x="size", y="tip")
-sns.displot(y)
+X = df.drop(["tip", "tip_pct"], axis=1)
+y = df["tip_pct"]
 
 
 #%%
@@ -44,9 +42,12 @@ pipeline = Pipeline(
         # ("model", LinearRegression()),
         (
             "model",
-            HistGradientBoostingRegressor(
-                loss="absolute_error",
-            ),
+            # HistGradientBoostingRegressor(
+            #     loss="absolute_error",
+            # ),
+            # DecisionTreeRegressor()
+            # DummyRegressor(strategy="mean"),
+            LGBMRegressor(objective="mae", importance_type="gain"),
         ),
     ]
 )
@@ -55,6 +56,12 @@ print(cross_val_score(pipeline, X, y, scoring="neg_mean_absolute_error").mean())
 preds = cross_val_predict(pipeline, X, y)
 
 #%%
+pred_df = X.assign(pred=preds, true=y, diff=preds - y)
+pred_df.sort_values("diff").tail(15)
+
+
+#%%
+
 # sns.scatterplot(x=y, y=preds - y, hue=df.day)
 sns.kdeplot(preds - y, hue=df.day)
 print(mean_absolute_error(y, preds))
@@ -72,4 +79,25 @@ px.scatter(data_frame=plotdf, x="tip", y="resid", hover_data=plotdf.columns, col
 from sklearn.inspection import PartialDependenceDisplay
 
 pipeline.fit(X, y)
-PartialDependenceDisplay.from_estimator(pipeline, X, features=["day"])
+PartialDependenceDisplay.from_estimator(
+    pipeline["model"],
+    _transformer.transform(X),
+    features=["remainder__total_bill"],
+    feature_names=_transformer.get_feature_names_out(),
+)
+
+
+#%%
+from sklearn.inspection import partial_dependence
+
+partial_dependence(
+    pipeline["model"],
+    X=pd.DataFrame(
+        _transformer.transform(X), columns=_transformer.get_feature_names_out()
+    ),
+    features=["oe__day"],
+)
+
+
+#%%
+from sklearn.pipeline import ma
